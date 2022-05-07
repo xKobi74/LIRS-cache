c396c016199457ea024cf316a3222aa44cca09a0
Анастасия Черникова <nastya.funnyant@yandex.ru>
2022-05-08 00:29:11 +0300

    Change functions due to creating a new structure list and stack

--- source/hash.c
+++ source/hash.c
@@ -4,6 +4,47 @@
 #include "hash.h"
 #include "stack.h"
 
+#if 0
+int main() {
+    
+    struct element_hash *element;
+    struct element_hash **hash = make_hash();
+    struct dlinked_list_element a;
+
+    change_stack(11, NULL, hash);
+    change_stack(6, NULL, hash);
+    change_stack(16, NULL, hash);
+    //element = new_element(3, NULL);
+
+    printf("adress 11: %p\n", (hash[1])->stack);
+    change_stack(11, &a, hash);
+    printf("adress 11: %p\n", (hash[1])->stack);
+    printf("adress 11: %p\n", element_in_stack(11, hash));
+
+
+    printf("%d\n", hash[1]->name);
+    printf("%d\n", (hash[1]->next)->name);
+    printf("%d\n\n", ((hash[1]->next)->next)->name);
+
+    delete_element_hash(6, hash);
+    printf("%d\n", delete_element_hash(13, hash));
+    printf("%d\n", delete_element_hash(11, hash));
+
+    //printf("%d\n", hash[1]->name);
+    //printf("%d\n", (hash[1]->next)->name);   
+
+    delete_element_hash(16, hash); 
+
+    //printf("%d\n", hash[1]->name);
+
+    //delete_element_hash(11, hash);
+
+    free(hash);
+
+    return 0;
+}
+#endif
+
 struct element_hash **make_hash() {
     
     struct element_hash **hash;
@@ -13,7 +54,7 @@ struct element_hash **make_hash() {
     return hash;
 }
 
-struct element_hash *new_element(int name, struct stack_element *address) {
+struct element_hash *new_element(int name, struct dlinked_list_element *address) {
     
     assert(address != NULL);
 
@@ -29,7 +70,7 @@ struct element_hash *new_element(int name, struct stack_element *address) {
 }
 
 
-void change_stack(int name, struct stack_element *address, struct element_hash **hash) {
+void change_in_hash(int name, struct dlinked_list_element *address, struct element_hash **hash, enum what_object object) {
 
     assert(hash != NULL);
     assert(address != NULL);
@@ -39,8 +80,14 @@ void change_stack(int name, struct stack_element *address, struct element_hash *
 
     while ((*current) != NULL) {
         if ((*current)->name == name) {
-            (*current)->stack = address;
-            return;
+            if (object == Stack) {
+                (*current)->stack = address;;
+                return;
+            }
+            else {
+                (*current)->list = address;
+                return;
+            }
         }
         current = &((*current)->next);
     }
@@ -49,27 +96,7 @@ void change_stack(int name, struct stack_element *address, struct element_hash *
 
 }
 
-void change_list(int name, struct list_element *address, struct element_hash **hash) {
-
-    assert(hash != NULL);
-    assert(address != NULL);
-
-    int number = name % len_hash;
-    struct element_hash *current = hash[number];
-
-    while (current != NULL) {
-        if (current->name == name) {
-            current->list = address;
-            return;
-        }
-        current = current->next;
-    }
-
-    assert(current != NULL);
-
-}
-
-struct stack_element *element_in_stack(int name, struct element_hash **hash) {
+struct dlinked_list_element *find_element(int name, struct element_hash **hash, enum what_object object) {
     
     assert(hash != NULL);
     
@@ -78,24 +105,12 @@ struct stack_element *element_in_stack(int name, struct element_hash **hash) {
 
     while (current != NULL) {
         if (current->name == name) {
-            return current->stack;
-        }
-        current = current->next;
-    }
-
-    return NULL;
-}
-
-struct list_element *element_in_list(int name, struct element_hash **hash) {
-    
-    assert(hash != NULL);
-
-    int number = name % len_hash;
-    struct element_hash *current = hash[number];
-
-    while (current != NULL) {
-        if (current->name == name) {
-            return current->list;
+            if (object == Stack) {
+                return current->stack;
+            }
+            else {
+                return current->list;
+            }
         }
         current = current->next;
     }
